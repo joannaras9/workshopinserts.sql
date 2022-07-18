c9e17e836b522ac83b101bbf6808517de4fa5487
joannaras9 <100958925+joannaras9@users.noreply.github.com>
2022-07-18 12:47:33 +0200

    Create workshop.sql

---
+++ workshop.sql
@@ -0,0 +1,282 @@
+ INSERT INTO Cust_type (customer_type_id, customer_type_name)
+ VALUES ( 1, 'billpay');
+ INSERT INTO Cust_type (customer_type_id, customer_type_name)
+ VALUES ( 2, 'payasyougo');
+ 
+ INSERT INTO Customer (customer_id, cust_fname, cust_lname, dob, mobile_number, email, fk_customer_type_id)
+VALUES (1, 'John', 'Smith',  '1995-05-08', 0871234501, 'j.smith@gmail.com', 1); 
+INSERT INTO Customer (customer_id, cust_fname, cust_lname, dob, mobile_number, email, fk_customer_type_id)
+VALUES (2, 'Sophia', 'Hernandez', '1993-04-28', 0877777602, 's.hernandez@gmail.com', 1);
+INSERT INTO Customer (customer_id, cust_fname, cust_lname, dob, mobile_number, email, fk_customer_type_id)
+VALUES (3, 'Wendy', 'Brown', '1970-11-11', 0877577603, 'w.brown@gmail.com', 1);
+INSERT INTO Customer (customer_id, cust_fname, cust_lname, dob, mobile_number, email, fk_customer_type_id)
+VALUES (4, 'Samantha', 'Kelly', '1986-01-13', 0877777604, 's.kelly@gmail.com', 1);
+INSERT INTO Customer (customer_id, cust_fname, cust_lname, dob, mobile_number, email, fk_customer_type_id)
+VALUES (5, 'Susan', 'Dunne', '1980-02-05', 0874477605, 's.dunne@gmail.com', 2);
+INSERT INTO Customer (customer_id, cust_fname, cust_lname, dob, mobile_number, email, fk_customer_type_id)
+VALUES (6, 'Gerard', 'Murphy', '1985-10-12', 0877771206, 'g.murphy@gmail.com', 2);
+INSERT INTO Customer (customer_id, cust_fname, cust_lname, dob, mobile_number, email, fk_customer_type_id)
+VALUES (7, 'Laura', 'Power', '1981-08-09', 0877777607, 'l.power@gmail.com', 2);
+INSERT INTO Customer (customer_id, cust_fname, cust_lname, dob, mobile_number, email, fk_customer_type_id)
+VALUES (8, 'John', 'Doyle', '1978-09-22', 0878877608, 'j.doyle@gmail.com', 2);
+INSERT INTO Customer (customer_id, cust_fname, cust_lname, dob, mobile_number, email, fk_customer_type_id)
+VALUES (9, 'Gary', ' Foster', '1979-01-21', 0878877609, 'gfoster@gmail.com', 1);
+INSERT INTO Customer (customer_id, cust_fname, cust_lname, dob, mobile_number, email, fk_customer_type_id)
+VALUES (10, 'Sherry' ,  'Gibbons','1960-06-06', 0878877610, 'sgibbons@gmail.com', 1);
+INSERT INTO Customer (customer_id, cust_fname, cust_lname, dob, mobile_number, email, fk_customer_type_id)
+VALUES (11, 'Jack', 'Nicholson', '1978-03-17', 0878877611, 'jacknick@gmail.com', 2);
+INSERT INTO Customer (customer_id, cust_fname, cust_lname, dob, mobile_number, email, fk_customer_type_id)
+VALUES (12, 'Penny', 'Green', '1977-11-04', 0878877612, 'pgreen@gmail.com', 2);
+INSERT INTO Customer (customer_id, cust_fname, cust_lname, dob, mobile_number, email, fk_customer_type_id)
+VALUES (13, 'Alexander', 'Evans', '1990-02-18', 0878877613, 'alexe@gmail.com', 1);
+INSERT INTO Customer (customer_id, cust_fname, cust_lname, dob, mobile_number, email, fk_customer_type_id)
+VALUES (14, 'Jeremy', 'White', '1955-07-08', 0878877614, 'j.white@gmail.com', 2);
+INSERT INTO Customer (customer_id, cust_fname, cust_lname, dob, mobile_number, email, fk_customer_type_id)
+VALUES (15, 'Omar', 'Badshah', '1970-09-11', 0878877615, 'omarb@gmail.com', 2);
+INSERT INTO Customer (customer_id, cust_fname, cust_lname, dob, mobile_number, email, fk_customer_type_id)
+VALUES (16, 'Preston', 'Harrison', '1982-02-22', 0878877616, 'p.harrison@gmail.com', 1);
+INSERT INTO Customer (customer_id, cust_fname, cust_lname, dob, mobile_number, email, fk_customer_type_id)
+VALUES (17, 'Manuel', 'Rodriguez', '1981-01-01', 0878877617, 'rodrmal@gmail.com', 2);
+INSERT INTO Customer (customer_id, cust_fname, cust_lname, dob, mobile_number, email, fk_customer_type_id)
+VALUES (18, 'David', 'Jones', '1991-04-04', 0878877618, 'jonesd@gmail.com', 2);
+INSERT INTO Customer (customer_id, cust_fname, cust_lname, dob, mobile_number, email, fk_customer_type_id)
+VALUES (19, 'Nick', 'Flanders', '1968-02-19', 0878877619, 'nick.fl@gmail.com', 1);
+INSERT INTO Customer (customer_id, cust_fname, cust_lname, dob, mobile_number, email, fk_customer_type_id)
+VALUES (20, 'Paul', 'Brown', '1957-10-24', 0878877620, 'p.brown@gmail.com', 1);
+INSERT INTO Customer (customer_id, cust_fname, cust_lname, dob, mobile_number, email, fk_customer_type_id)
+VALUES (21, 'Sara', 'Rehm', '1954-12-08', 0878877621, 's.rehm@gmail.com', 1);
+INSERT INTO Customer (customer_id, cust_fname, cust_lname, dob, mobile_number, email, fk_customer_type_id)
+VALUES (22, 'Haley', 'Carter', '1944-05-01', 0878877622, 'h.carter@gmail.com', 2);
+INSERT INTO Customer (customer_id, cust_fname, cust_lname, dob, mobile_number, email, fk_customer_type_id)
+VALUES (23, 'Julian', 'Thomas', '1976-01-27', 0878877623, 'j.thomas@gmail.com', 2);
+INSERT INTO Customer (customer_id, cust_fname, cust_lname, dob, mobile_number, email, fk_customer_type_id)
+VALUES (24, 'George', 'Lucas', '1988-07-15', 0878877624, 'glucas@gmail.com', 2);
+INSERT INTO Customer (customer_id, cust_fname, cust_lname, dob, mobile_number, email, fk_customer_type_id)
+VALUES (25, 'Perry', 'Jordan', '1986-11-13', 0878877625, 'jorganp@gmail.com', 1);
+INSERT INTO Customer (customer_id, cust_fname, cust_lname, dob, mobile_number, email, fk_customer_type_id)
+VALUES (26, 'Christopher', 'Layton', '1987-03-03', 0878877626, 'chrislay@gmail.com', 2);
+INSERT INTO Customer (customer_id, cust_fname, cust_lname, dob, mobile_number, email, fk_customer_type_id)
+VALUES (27, 'Devon', 'Myers','1982-06-17', 0878877627, 'd.myers@gmail.com', 2);
+INSERT INTO Customer (customer_id, cust_fname, cust_lname, dob, mobile_number, email, fk_customer_type_id)
+VALUES (28, 'Debra', 'Talkington', '1988-10-28', 0878877628, 'debra.t@gmail.com', 2);
+INSERT INTO Customer (customer_id, cust_fname, cust_lname, dob, mobile_number, email, fk_customer_type_id)
+VALUES (29, 'Peter', 'Johnson', '1981-09-08', 0878877629, 'p.johnson@gmail.com', 1);
+INSERT INTO Customer (customer_id, cust_fname, cust_lname, dob, mobile_number, email, fk_customer_type_id)
+VALUES (30, 'Harry', 'Brown', '1985-10-05', 0878877630, 'harry.brown@gmail.com', 2);
+
+
+
+INSERT INTO Addresses (fk_customer_id, address, city, state_code, zip) 
+VALUES (1, '32 Hungerford Ave', 'Adams', 'NY', 13605);
+INSERT INTO Addresses (fk_customer_id, address, city, state_code, zip) 
+VALUES (2, '342 Tunafish Lane', 'Raleigh', 'NC', 27606);
+INSERT INTO Addresses (fk_customer_id, address, city, state_code, zip) 
+VALUES (3, '43 Crab Orchard Dr', 'Oakland', 'CA', 90210);
+INSERT INTO Addresses (fk_customer_id, address, city, state_code, zip) 
+VALUES (4, '423 Wisteria Lane', 'Simmersville', 'AK', 54112);
+INSERT INTO Addresses (fk_customer_id, address, city, state_code, zip) 
+VALUES (5, '673 Greenwich Ave', 'Newark', 'NJ', 80432);
+INSERT INTO Addresses (fk_customer_id, address, city, state_code, zip) 
+VALUES (6, '2134 Grant St', 'Gary', 'IN', 47404);
+INSERT INTO Addresses (fk_customer_id, address, city, state_code, zip) 
+VALUES (7, '1568 Greenfield Ave', 'Augusta', 'GA', 30568);
+INSERT INTO Addresses (fk_customer_id, address, city, state_code, zip) 
+VALUES (8, '8725 Black St', 'Philadelphia', 'PA', 19603);
+INSERT INTO Addresses (fk_customer_id, address, city, state_code, zip) 
+VALUES (9, '3752 Wittfield Blvd', 'Indianapolis', 'IN', 46219);
+INSERT INTO Addresses (fk_customer_id, address, city, state_code, zip) 
+VALUES (10, '5517 Oak St', 'Phoenix', 'AZ', 85072);
+INSERT INTO Addresses (fk_customer_id, address, city, state_code, zip) 
+VALUES (11, '1234 Star Lane', 'Hollywood', 'CA', 90210);
+INSERT INTO Addresses (fk_customer_id, address, city, state_code, zip) 
+VALUES (12, '4972 Massachusetts Ave', 'Orlando', 'FL', 33718);
+INSERT INTO Addresses (fk_customer_id, address, city, state_code, zip) 
+VALUES (13, '6310 E 21st Rd', 'Las Vegas', 'NV', 83594);
+INSERT INTO Addresses (fk_customer_id, address, city, state_code, zip) 
+VALUES (14, '3954 Brentwood Dr', 'Seattle', 'WA', 99037);
+INSERT INTO Addresses (fk_customer_id, address, city, state_code, zip) 
+VALUES (15, '6801 Regina Cir', 'Madison', 'WI', 53209);
+INSERT INTO Addresses (fk_customer_id, address, city, state_code, zip) 
+VALUES (16, '104 Main St', 'Denver', 'CO', 81712);
+INSERT INTO Addresses (fk_customer_id, address, city, state_code, zip) 
+VALUES (17, '99543 Westin Blvd', 'Provo', 'UT', 85478);
+INSERT INTO Addresses (fk_customer_id, address, city, state_code, zip) 
+VALUES (18, '5488 W 34th St', 'Boston', 'MA', 2104);
+INSERT INTO Addresses (fk_customer_id, address, city, state_code, zip) 
+VALUES (19, '3486 Happy Trails Dr', 'Springfield', 'OH', 45872);
+INSERT INTO Addresses (fk_customer_id, address, city, state_code, zip) 
+VALUES (20, '3290 Pennsylvania Ave', 'Chicago', 'IL', 61208);
+INSERT INTO Addresses (fk_customer_id, address, city, state_code, zip) 
+VALUES (21, '7746 Wysong Ave', 'Detroit', 'MI', 48913);
+INSERT INTO Addresses (fk_customer_id, address, city, state_code, zip) 
+VALUES (22, '2957 Princess Way', 'Portland', 'OR', 97532);
+INSERT INTO Addresses (fk_customer_id, address, city, state_code, zip) 
+VALUES (23, '5564 Dandy Trail', 'Santa Fe', 'NM', 81543);
+INSERT INTO Addresses (fk_customer_id, address, city, state_code, zip) 
+VALUES (24, '8224 Star Wars Ln', 'Galaxy', 'TN', 37512);
+INSERT INTO Addresses (fk_customer_id, address, city, state_code, zip) 
+VALUES (25, '4321 Victory Rd', 'Montgomery', 'AL', 33219);
+INSERT INTO Addresses (fk_customer_id, address, city, state_code, zip) 
+VALUES (26, '871 Georgia Pl', 'Houston', 'TX', 78704);
+INSERT INTO Addresses (fk_customer_id, address, city, state_code, zip) 
+VALUES (27, '9932 Carmel Dr', 'New York', 'NY', 11920);
+INSERT INTO Addresses (fk_customer_id, address, city, state_code, zip) 
+VALUES (28, '8421 Smithfield Ave', 'Mesa', 'AZ', 82573);
+INSERT INTO Addresses (fk_customer_id, address, city, state_code, zip) 
+VALUES (29, '4213 Jones Pkwy', 'Missoula', 'MS', 37228);
+INSERT INTO Addresses (fk_customer_id, address, city, state_code, zip)
+VALUES (30, '125 Cinnamon Way', 'Raleigh', 'NC', 27418);
+
+
+INSERT INTO Manufacturer(manu_name) VALUES('Apple');
+INSERT INTO Manufacturer(manu_name) VALUES('Samsung');
+INSERT INTO Manufacturer(manu_name) VALUES('Huawei');
+INSERT INTO Manufacturer(manu_name) VALUES('Nokia');
+INSERT INTO Manufacturer(manu_name) VALUES('Sony');
+
+INSERT INTO Product(product_id, product_code, product_name, price_bp, price_pg, quantity, fk_manu_id ) 
+VALUES (1, 'iph8', 'iphone 8 64BG', 599.00, 899.00,  10, 1);
+INSERT INTO Product(product_id, product_code, product_name, price_bp, price_pg, quantity, fk_manu_id ) 
+VALUES (2,'iph8+', 'iphone 8 plus 64BG', 699.00, 950.00, 10, 1);
+INSERT INTO Product(product_id,product_code, product_name, price_bp, price_pg, quantity, fk_manu_id ) 
+VALUES (3, 'iph7', 'iphone 7 32BG', 499.00, 599.00, 10, 1);
+INSERT INTO Product(product_id,product_code, product_name, price_bp, price_pg, quantity, fk_manu_id )
+VALUES (4,'iph7+', 'iphone 7 plus 32BG', 600.00, 900.00, 10, 1);
+INSERT INTO Product(product_id,product_code, product_name, price_bp, price_pg, quantity, fk_manu_id )
+VALUES (5,'iph6s', 'iphone 6s 32BG', 150.00, 500.00, 10, 1);
+INSERT INTO Product(product_id,product_code, product_name, price_bp, price_pg, quantity, fk_manu_id )  
+VALUES (6,'sam8', 'samsung note 8', 650.00, 950.00, 10, 2);
+INSERT INTO Product(product_id,product_code, product_name, price_bp, price_pg, quantity, fk_manu_id )
+VALUES (7,'sams8', 'samsung s8 64BG', 450.00, 750.00, 10, 2);
+INSERT INTO Product(product_id,product_code, product_name, price_bp, price_pg, quantity, fk_manu_id )  
+VALUES (8,'sama5', 'samsung a5 2017', 150.00, 350.00, 10, 2);
+INSERT INTO Product(product_id,product_code, product_name, price_bp, price_pg, quantity, fk_manu_id )
+VALUES (9,'sama3', 'samsung a3 2017', 50.00, 250.00, 10, 2);
+INSERT INTO Product(product_id,product_code, product_name, price_bp, price_pg, quantity, fk_manu_id )
+VALUES (10,'samj5', 'samsung j5 2017', 50.00, 230.00, 10, 2);
+INSERT INTO Product(product_id,product_code, product_name, price_bp, price_pg, quantity, fk_manu_id )
+VALUES (11,'huawp10', 'huawei p10', 200.00,500.00, 10, 3);
+INSERT INTO Product(product_id,product_code, product_name, price_bp, price_pg, quantity, fk_manu_id )
+VALUES (12,'huawp9', 'huawei p9',100.00, 400.00, 10, 3);
+INSERT INTO Product(product_id,product_code, product_name, price_bp, price_pg, quantity, fk_manu_id )
+VALUES (13,'huawpp8l', 'huawei p8 lite', 50.00, 170.00, 10, 3);
+INSERT INTO Product(product_id,product_code, product_name, price_bp, price_pg, quantity, fk_manu_id )
+VALUES (14,'huawy6', 'huawei y6', 00.00, 130.00, 5, 3);
+INSERT INTO Product(product_id,product_code, product_name, price_bp, price_pg, quantity, fk_manu_id )
+VALUES (15,'nok3', 'nokia 3', 00.00, 100, 10, 4);
+INSERT INTO Product(product_id,product_code, product_name, price_bp, price_pg, quantity, fk_manu_id )
+VALUES (16,'nok3', 'nokia 3', 50.00, 120.00, 10, 4);
+INSERT INTO Product(product_id,product_code, product_name, price_bp, price_pg, quantity, fk_manu_id )
+VALUES (17,'nok3', 'nokia 3', 50.00, 99.00, 10, 4);
+INSERT INTO Product(product_id,product_code, product_name, price_bp, price_pg, quantity, fk_manu_id )  
+VALUES (18,'sxz', 'sony xz', 149.00, 399.00, 10, 5);
+INSERT INTO Product(product_id,product_code, product_name, price_bp, price_pg, quantity, fk_manu_id )
+VALUES (19,'sxz1', 'sony xz1', 100.00, 350.00, 10, 5);
+INSERT INTO Product(product_id,product_code, product_name, price_bp, price_pg, quantity, fk_manu_id )
+VALUES (20,'sxa1', 'sony xa1', 30.00, 249.00, 10, 5);
+  
+  
+ INSERT INTO Order_det (order_id, fk_customer_id )
+ VALUES (1,1);
+ INSERT INTO Order_det (order_id, fk_customer_id )
+ VALUES (2,2);
+ INSERT INTO Order_det (order_id, fk_customer_id )
+ VALUES (3,3);
+ INSERT INTO Order_det (order_id, fk_customer_id )
+ VALUES (4,4);
+ INSERT INTO Order_det (order_id, fk_customer_id )
+ VALUES (5,5);
+ INSERT INTO Order_det (order_id, fk_customer_id )
+ VALUES (6,6);
+ INSERT INTO Order_det (order_id, fk_customer_id )
+ VALUES (7,7);
+ INSERT INTO Order_det (order_id, fk_customer_id )
+ VALUES (8,8);
+ INSERT INTO Order_det (order_id, fk_customer_id )
+ VALUES (9,9);
+ INSERT INTO Order_det (order_id, fk_customer_id )
+ VALUES (10,10);
+ INSERT INTO Order_det (order_id, fk_customer_id )
+ VALUES (11,11);
+ INSERT INTO Order_det (order_id, fk_customer_id )
+ VALUES (12,12);
+ INSERT INTO Order_det (order_id, fk_customer_id )
+ VALUES (13,13);
+ INSERT INTO Order_det (order_id, fk_customer_id )
+ VALUES (14,14);
+ INSERT INTO Order_det (order_id, fk_customer_id )
+ VALUES (15,15);
+ INSERT INTO Order_det (order_id, fk_customer_id )
+ VALUES (16,16);
+ INSERT INTO Order_det (order_id, fk_customer_id )
+ VALUES (17,17);
+ INSERT INTO Order_det (order_id, fk_customer_id )
+ VALUES (18,18);
+ INSERT INTO Order_det (order_id, fk_customer_id )
+ VALUES (19,19);
+ INSERT INTO Order_det (order_id, fk_customer_id )
+ VALUES (20,20);
+  INSERT INTO Order_det (order_id, fk_customer_id )
+ VALUES (21,21);
+ INSERT INTO Order_det (order_id, fk_customer_id )
+ VALUES (22,22);
+ INSERT INTO Order_det (order_id, fk_customer_id )
+ VALUES (23,23);
+ INSERT INTO Order_det (order_id, fk_customer_id )
+ VALUES (24,24);
+ INSERT INTO Order_det (order_id, fk_customer_id )
+ VALUES (25,25);
+ INSERT INTO Order_det (order_id, fk_customer_id )
+ VALUES (26,26);
+ INSERT INTO Order_det (order_id, fk_customer_id )
+ VALUES (27,27);
+ INSERT INTO Order_det (order_id, fk_customer_id )
+ VALUES (28,28);
+ INSERT INTO Order_det (order_id, fk_customer_id )
+ VALUES (29,29);
+ INSERT INTO Order_det (order_id, fk_customer_id )
+ VALUES (30,30);
+
+
+
+INSERT INTO Product(product_id, product_code, product_name, price_bp, price_pg, quantity, fk_manu_id ) 
+VALUES (1, 'iph8', 'iphone 8 64BG', 599.00, 899.00,  10, 1);
+INSERT INTO Product(product_id, product_code, product_name, price_bp, price_pg, quantity, fk_manu_id ) 
+VALUES (2,'iph8+', 'iphone 8 plus 64BG', 699.00, 950.00, 10, 1);
+INSERT INTO Product(product_id,product_code, product_name, price_bp, price_pg, quantity, fk_manu_id ) 
+VALUES (3, 'iph7', 'iphone 7 32BG', 499.00, 599.00, 10, 1);
+INSERT INTO Product(product_id,product_code, product_name, price_bp, price_pg, quantity, fk_manu_id )
+VALUES (4,'iph7+', 'iphone 7 plus 32BG', 600.00, 900.00, 10, 1);
+INSERT INTO Product(product_id,product_code, product_name, price_bp, price_pg, quantity, fk_manu_id )
+VALUES (5,'iph6s', 'iphone 6s 32BG', 150.00, 500.00, 10, 1);
+INSERT INTO Product(product_id,product_code, product_name, price_bp, price_pg, quantity, fk_manu_id )  
+VALUES (6,'sam8', 'samsung note 8', 650.00, 950.00, 10, 2);
+INSERT INTO Product(product_id,product_code, product_name, price_bp, price_pg, quantity, fk_manu_id )
+VALUES (7,'sams8', 'samsung s8 64BG', 450.00, 750.00, 10, 2);
+INSERT INTO Product(product_id,product_code, product_name, price_bp, price_pg, quantity, fk_manu_id )  
+VALUES (8,'sama5', 'samsung a5 2017', 150.00, 350.00, 10, 2);
+INSERT INTO Product(product_id,product_code, product_name, price_bp, price_pg, quantity, fk_manu_id )
+VALUES (9,'sama3', 'samsung a3 2017', 50.00, 250.00, 10, 2);
+INSERT INTO Product(product_id,product_code, product_name, price_bp, price_pg, quantity, fk_manu_id )
+VALUES (10,'samj5', 'samsung j5 2017', 50.00, 230.00, 10, 2);
+INSERT INTO Product(product_id,product_code, product_name, price_bp, price_pg, quantity, fk_manu_id )
+VALUES (11,'huawp10', 'huawei p10', 200.00,500.00, 10, 3);
+INSERT INTO Product(product_id,product_code, product_name, price_bp, price_pg, quantity, fk_manu_id )
+VALUES (12,'huawp9', 'huawei p9',100.00, 400.00, 10, 3);
+INSERT INTO Product(product_id,product_code, product_name, price_bp, price_pg, quantity, fk_manu_id )
+VALUES (13,'huawpp8l', 'huawei p8 lite', 50.00, 170.00, 10, 3);
+INSERT INTO Product(product_id,product_code, product_name, price_bp, price_pg, quantity, fk_manu_id )
+VALUES (14,'huawy6', 'huawei y6', 00.00, 130.00, 5, 3);
+INSERT INTO Product(product_id,product_code, product_name, price_bp, price_pg, quantity, fk_manu_id )
+VALUES (15,'nok3', 'nokia 3', 00.00, 100, 10, 4);
+INSERT INTO Product(product_id,product_code, product_name, price_bp, price_pg, quantity, fk_manu_id )
+VALUES (16,'nok3', 'nokia 3', 50.00, 120.00, 10, 4);
+INSERT INTO Product(product_id,product_code, product_name, price_bp, price_pg, quantity, fk_manu_id )
+VALUES (17,'nok3', 'nokia 3', 50.00, 99.00, 10, 4);
+INSERT INTO Product(product_id,product_code, product_name, price_bp, price_pg, quantity, fk_manu_id )  
+VALUES (18,'sxz', 'sony xz', 149.00, 399.00, 10, 5);
+INSERT INTO Product(product_id,product_code, product_name, price_bp, price_pg, quantity, fk_manu_id )
+VALUES (19,'sxz1', 'sony xz1', 100.00, 350.00, 10, 5);
+INSERT INTO Product(product_id,product_code, product_name, price_bp, price_pg, quantity, fk_manu_id )
+VALUES (20,'sxa1', 'sony xa1', 30.00, 249.00, 10, 5);
+  
